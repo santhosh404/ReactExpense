f9eeaf76dfe5fd14bf1feb49bed25e1d0f2b34cd
santhosh404 <santhoshmathi2002@gmail.com>
2023-05-14 19:38:53 +0530

    final

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000/api';
+const API_URL = 'https://reactexpense.onrender.com/api';
 
 
 export const register = async (firstName, lastName, email, password) => {
